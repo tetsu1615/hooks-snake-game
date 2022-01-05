953ff9f0cbc076336714e52d77ea267afb62cd75
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2022-01-06 00:05:56 +0900

    01-05

---
+++ src/components/Field.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Field = () => {
+  return (
+    <div className="field">
+      {
+        new Array(35 * 35).fill('').map(() => <div className="dots"></div>)
+      }
+    </div>
+  );
+};
+
+export default Field;
