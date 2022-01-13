8facb3feba255323fdea456978bebb1e6e17eec1
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2022-01-13 20:26:34 +0900

    01-13

--- src/App.js
+++ src/App.js
@@ -7,14 +7,48 @@ import { initFields } from './utils'
 
 const initialPosition = { x: 17, y: 17 }
 const initialValues = initFields(35, initialPosition)
+const defaultInterval = 100
+
+const GameStatus = Object.freeze({
+  init: 'init',
+  playing: 'playing',
+  suspended: 'suspended',
+  gameover: 'gameover'
+})
+
+let timer = undefined
+
+const unsubscribe = () => {
+  if (!timer) {
+    return
+  }
+  clearInterval(timer)
+}
+
 
 function App() {
-  const [fields, setFields] = useState(initFields)
+  const [fields, setFields] = useState(initialValues)
   const [position, setPosition] = useState()
+  const [status, setStatus] = useState(GameStatus.init)
+  const [tick, setTick] = useState(0)
 
   useEffect(() => {
     setPosition(initialPosition)
+    // ゲームの中の時間を管理する
+    timer = setInterval(() => {
+      setTick(tick => tick + 1)
+    }, defaultInterval)
+    return unsubscribe
   }, [])
+
+  useEffect(() => {
+    if (!position || status !== GameStatus.playing) {
+      return
+    }
+    goUp()
+  }, [tick])
+
+  const onStart = () => setStatus(GameStatus.playing)
 
   const goUp = () => {
     const { x, y } = position
@@ -36,11 +70,8 @@ function App() {
       <main className="main">
         <Field fields={fields} />
       </main>
-      <div style={{ padding: '16px' }}>
-        <button onClick={goUp}>進む</button>
-      </div>
       <footer className="footer">
-        <Button />
+        <Button onStart={onStart} />
         <ManipulationPanel />
       </footer>
     </div>
